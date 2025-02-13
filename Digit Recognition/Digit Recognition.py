12799d044be4f40eab3f264c36ca449c758797b5
Hsin-Ya (Annie) Chien <a0988115306@gmail.com>
2025-02-13 17:51:24 +0800

    Update Digit Recognition.py

--- Digit Recognition/Digit Recognition.py
+++ Digit Recognition/Digit Recognition.py
@@ -1,8 +1,8 @@
-# 載入 MNIST 的 Keras 資料集模組
+# 載入 MNIST 的 Keras datasets
 from tensorflow.keras.datasets import mnist
 (x_train, y_train), (x_valid, y_valid) = mnist.load_data()
 
-# 正規化影像資料
+# Data normalization
 x_train = x_train / 255
 x_valid = x_valid / 255
 
@@ -17,47 +17,46 @@ y_valid = keras.utils.to_categorical(y_valid, num_categories)
 from tensorflow.keras.models import Sequential
 model = Sequential()
 
-# 建立輸入層
+# construct input Layer
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
-# 第一卷積層(32個3x3的卷積核， 步長為1)
+# First convolution layer (32個3x3的卷積核， 步長為1)
 model.add(Conv2D(filters=32, kernel_size=(3, 3), strides=(1, 1), padding='valid', activation='relu', input_shape = (28, 28, 1)))
 
-# 建立隱藏層
-# 第一池化層
+# buld hidden layer
+# First pooling layer
 model.add(MaxPooling2D(pool_size=(2, 2), strides = (2, 2)))
 
-# 第二卷積層（64個5x5的卷積核， 步長為1，使用填充）
+# Second convolution layer（64個5x5的卷積核， 步長為1，使用填充）
 model.add(Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
 
-# 第二池化層
+# Second pooling layer
 model.add(MaxPooling2D(pool_size=(2, 2), strides = (2, 2)))
 
-# 後續卷積層
+# subsequent convolution layer
 model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
 model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
 model.add(Conv2D(128, (3, 3), activation='relu', padding='valid'))
 
-# 最後池化層
+# The last pooling layer
 model.add(MaxPooling2D(pool_size=(2, 2), strides = (2, 2)))
 
-# 全連接層
+# Fully Connected layer
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dense(128, activation='relu'))
 model.add(Dense(10, activation='softmax'))
 
-# 編譯模型
+# compiling and summary
 model.compile(loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
-# 輸出模型架構
 model.summary()
 
-# 訓練模型
+# training model
 history = model.fit(
     x_train, y_train, epochs = 5, verbose = 1, validation_data = (x_valid, y_valid)
 )
 
-# 觀察準確度
+# check accuracy
 from matplotlib import pyplot as plt
 history = history.history
 plt.xlabel('Epochs')
